a5d1231ece67704b5af2fac2a71e8d56e9505a39
Anas MD <ansal@Anass-MacBook-Pro.local>
2017-04-01 01:56:12 +0530

    adding filters page and menu

--- app/src/main/java/com/five/shubhamagarwal/five/InterestActivity.java
+++ app/src/main/java/com/five/shubhamagarwal/five/InterestActivity.java
@@ -2,6 +2,7 @@ package com.five.shubhamagarwal.five;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 
 public class InterestActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class InterestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
