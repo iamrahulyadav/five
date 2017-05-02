c1571214bb938fecae0ee10fc352e6a252e3427b
Anas MD <anas@getblueshift.com>
2017-05-02 18:18:22 +0530

    bug fix

--- app/src/main/java/com/spate/in/activities/LoginActivity.java
+++ app/src/main/java/com/spate/in/activities/LoginActivity.java
@@ -66,7 +66,7 @@ public class LoginActivity extends Activity implements BaseSliderView.OnSliderCl
             Gen.handleNotification(extras);
         } else if (extras != null && extras.getString(Constants.SHOW_LOGOUT_SCREEN, null) != null) {
             // show logout screen
-        } else {
+        } else if (!Gen.getUserIdFromLocalStorage().equals("")) {
             updateUserDataToBackend();
         }
 
